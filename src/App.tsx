a564db3c3fc2768d147a1ac7f7b7e3d757d3bd18
Shoaib Huq <slimsho123@gmail.com>
2025-01-15 00:17:06 -0600

    added photography page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-//Components
-import NavBar from "./components/NavBar";
-import AboutMe from "./components/sections/AboutMe";
-import Projects from "./components/sections/Projects";
-import Footer from "./components/Footer";
+// Components
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/sections/HomePage";
+import Photography from "./components/sections/Photography";
 
 function App() {
+  const host = window.location.hostname;
+  const subdomain = host.split(".")[0];
+
   return (
-    <body className="bg-black h-screen">
-      <div className="bg-black h-screen">
-        <header className="fixed inset-x-0 top-0 z-50">
-          <NavBar />
-        </header>
-        <main className="bg-black ">
-          <AboutMe />
-          <Projects />
-        </main>
-        <footer className="bg-black">
-          <Footer />
-        </footer>
-      </div>
-    </body>
+    <Router>
+      <Routes>
+        {subdomain === "photography" ? (
+          <Route path="/" element={<Photography />} />
+        ) : (
+          <Route path="/" element={<HomePage />} />
+        )}
+      </Routes>
+    </Router>
   );
 }
 
