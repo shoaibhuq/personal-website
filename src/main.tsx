bbdfa6191d5f81c50e05eb4edd5d194eecac844d
Shoaib Huq <slimsho123@gmail.com>
2025-01-16 16:05:01 -0600

    added photo routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Photography from "./components/sections/Photography";
 import NotFound from "./components/sections/NotFound";
 import "./index.css";
 import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     errorElement: <NotFound />,
+  },
+  {
+    path: "/photography",
+    element: <Photography />,
   },
 ]);
 
